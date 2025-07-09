b65d3be3e5b8302ee24630c5b45d54982d65e477
Dm-Rb <dm.rb.418http@gmail.com>
2025-07-09 11:58:34 +0300

    alpha 1.2 - ai chat bot (deep seek)

--- ai_client.py
+++ ai_client.py
@@ -29,31 +29,36 @@ class DeepSeekAPIClient:
             await self._session.close()
 
     async def request_post(self, messages_history: list, ai_role: str, user_request: str):
-        session = await self.get_session()
-        if not messages_history:
-            messages_history = [
-                {"role": "system", "content": "You are a helpful assistant." if not ai_role else ai_role},
-                {"role": "user", "content": user_request}
-            ]
-        else:
-            messages_history.append(
-                {"role": "user", "content": user_request}
-            )
-        body_ = {
-            "model": "deepseek-chat",
-            "temperature": self.temperature,
-            "messages": messages_history,
-            "stream": False
-        }
-        # url Только эндпоинт (сессия добавит base_url)
-        async with session.post(self.API_ENDPOINT, json=body_, headers=self.headers) as resp:
-            resp.raise_for_status()  # Проверяем статус ответа
-            response_json = await resp.json()
+        try:
+            session = await self.get_session()
+            if not messages_history:
+                messages_history = [
+                    {"role": "system", "content": "You are a helpful assistant." if not ai_role else ai_role},
+                    {"role": "user", "content": user_request}
+                ]
+            else:
+                messages_history.append(
+                    {"role": "user", "content": user_request}
+                )
+            body_ = {
+                "model": "deepseek-chat",
+                "temperature": self.temperature,
+                "messages": messages_history,
+                "stream": False
+            }
+            # url Только эндпоинт (сессия добавит base_url)
+            async with session.post(self.API_ENDPOINT, json=body_, headers=self.headers) as resp:
+                resp.raise_for_status()  # Проверяем статус ответа
+                response_json = await resp.json()
 
-            if not response_json.get('choices', None):
-                raise ValueError("The API response does not contain the 'choices' key")
-            for choice_item in response_json['choices']:
-                return choice_item['message']['content']
-
+                if not response_json.get('choices', None):
+                    raise ValueError("The API response does not contain the 'choices' key")
+                for choice_item in response_json['choices']:
+                    return choice_item['message']['content']
+        except Exception as e:
+            logger.error(
+                f"f.request_post error: {e}\n",
+                exc_info=True)
+            raise e
 
 deep_seek = DeepSeekAPIClient(config.ai_token)
