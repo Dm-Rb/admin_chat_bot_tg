9ae7c4ce7493a4ac1fec2f9955bb5a6c46d26a57
Dm-Rb <135587971+Dm-Rb@users.noreply.github.com>
2025-07-11 13:08:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,6 @@ class Config:
     api_id = os.getenv("API_ID")
     api_hash = os.getenv("API_HASH")
     phone_number = os.getenv("PHONE")
-    total_wipe_confirms = int(os.getenv("TOTAL_WIPE_CONFIRMS"))
-    personal_wipe_confirms = int(os.getenv("PERSONAL_WIPE_CONFIRMS"))
-    folder_4_json = 'export_json_files'
     ai_token = os.getenv("AI_TOKEN")
     ai_temperature = float(os.getenv("AI_TEMPERATURE"))
 
